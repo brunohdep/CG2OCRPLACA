d77c1c5fd27f3e2b5f5514bd276d83ffa662e089
brunohdep <brunohdep@gmail.com>
2016-06-16 08:26:48 -0300

    backup
    backup

--- OCR/OCRTest/FormMain.cs
+++ OCR/OCRTest/FormMain.cs
@@ -22,20 +22,11 @@ namespace OCRTest
         public FormMain()
         {
             InitializeComponent();
-<<<<<<< HEAD
             //String ip = "172.16.102.13";
             //t1 = new TcpListener(ip, 4243);
             //cliente = default(TcpClient);
             //t1.Start();
             //cliente = t1.AcceptTcpClient();
-=======
-            String ip = "172.16.102.13";
-            t1 = new TcpListener(ip, 4243);
-            cliente = default(TcpClient);
-            t1.Start();
-            cliente = t1.AcceptTcpClient();
->>>>>>> d42502b7af47b1297d378bdee8a3befcd18cb81b
-
         }
 
         TcpListener t1;
@@ -102,15 +93,7 @@ namespace OCRTest
             newImage = new AForge.Imaging.Filters.Median().Apply(newImage);
             newImage = new AForge.Imaging.Filters.Erosion().Apply(newImage);
             newImage = new AForge.Imaging.Filters.Erosion().Apply(newImage);
-<<<<<<< HEAD
-=======
-            //newImage = filter.Apply(newImage);
-            // newImage = filter.Apply(newImage);
-            // Erosion filter2 = new Erosion();
-            // newImage = filter2.Apply(newImage);
-            //newImage = filter2.Apply(newImage);
-            // newImage = filter2.Apply(newImage);
->>>>>>> d42502b7af47b1297d378bdee8a3befcd18cb81b
+
             return newImage;
         }
 
