235d629f25d95e63241319cb8e9fcf8c19f6a859
brunohdep <brunohdep@gmail.com>
2016-06-16 07:53:04 -0300

    Adicionado o projeto de placas

---
+++ OCR/OCRTest/FormMain.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace OCRTest
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripDropDownButton1 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.crop1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.crop2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.processarFiltrosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cinzaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.Filter = "Image Files|*.*|JPEG Files (*.jpeg)|*.jpeg|PNG Files (*.png)|*.png|JPG Files (*.j" +
+    "pg)|*.jpg|GIF Files (*.gif)|*.gif\"";
+            this.openFileDialog.Title = "Select Figure";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 45);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.pictureBox1);
+            this.splitContainer1.Panel1MinSize = 200;
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.textBox1);
+            this.splitContainer1.Panel2MinSize = 200;
+            this.splitContainer1.Size = new System.Drawing.Size(656, 415);
+            this.splitContainer1.SplitterDistance = 330;
+            this.splitContainer1.TabIndex = 5;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(328, 413);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(320, 413);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripButton2,
+            this.toolStripButton3,
+            this.toolStripDropDownButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(661, 38);
+            this.toolStrip1.TabIndex = 7;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.Image = global::OCRTest.Properties.Resources.fileopen;
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(56, 35);
+            this.toolStripButton1.Text = "Carregar";
+            this.toolStripButton1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.toolStripButton1.ToolTipText = "Carregar";
+            this.toolStripButton1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.Image = global::OCRTest.Properties.Resources.kview;
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(61, 35);
+            this.toolStripButton2.Text = "Processar";
+            this.toolStripButton2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.toolStripButton2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // toolStripButton3
+            // 
+            this.toolStripButton3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton3.Image = global::OCRTest.Properties.Resources.kview;
+            this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton3.Name = "toolStripButton3";
+            this.toolStripButton3.RightToLeftAutoMirrorImage = true;
+            this.toolStripButton3.Size = new System.Drawing.Size(23, 35);
+            this.toolStripButton3.Text = "Detectar Objetos";
+            this.toolStripButton3.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.toolStripButton3.Click += new System.EventHandler(this.toolStripButton3_Click);
+            // 
+            // toolStripDropDownButton1
+            // 
+            this.toolStripDropDownButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripDropDownButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.crop1ToolStripMenuItem,
+            this.crop2ToolStripMenuItem,
+            this.processarFiltrosToolStripMenuItem,
+            this.cinzaToolStripMenuItem});
+            this.toolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripDropDownButton1.Name = "toolStripDropDownButton1";
+            this.toolStripDropDownButton1.Size = new System.Drawing.Size(13, 35);
+            this.toolStripDropDownButton1.Text = "toolStripDropDownButton1";
+            // 
+            // crop1ToolStripMenuItem
+            // 
+            this.crop1ToolStripMenuItem.Name = "crop1ToolStripMenuItem";
+            this.crop1ToolStripMenuItem.Size = new System.Drawing.Size(159, 22);
+            this.crop1ToolStripMenuItem.Text = "Crop1";
+            this.crop1ToolStripMenuItem.Click += new System.EventHandler(this.crop1ToolStripMenuItem_Click);
+            // 
+            // crop2ToolStripMenuItem
+            // 
+            this.crop2ToolStripMenuItem.Name = "crop2ToolStripMenuItem";
+            this.crop2ToolStripMenuItem.Size = new System.Drawing.Size(159, 22);
+            this.crop2ToolStripMenuItem.Text = "Crop2";
+            this.crop2ToolStripMenuItem.Click += new System.EventHandler(this.crop2ToolStripMenuItem_Click);
+            // 
+            // processarFiltrosToolStripMenuItem
+            // 
+            this.processarFiltrosToolStripMenuItem.Name = "processarFiltrosToolStripMenuItem";
+            this.processarFiltrosToolStripMenuItem.Size = new System.Drawing.Size(159, 22);
+            this.processarFiltrosToolStripMenuItem.Text = "Processar Filtros";
+            this.processarFiltrosToolStripMenuItem.Click += new System.EventHandler(this.processarFiltrosToolStripMenuItem_Click);
+            // 
+            // cinzaToolStripMenuItem
+            // 
+            this.cinzaToolStripMenuItem.Name = "cinzaToolStripMenuItem";
+            this.cinzaToolStripMenuItem.Size = new System.Drawing.Size(159, 22);
+            this.cinzaToolStripMenuItem.Text = "Cinza";
+            this.cinzaToolStripMenuItem.Click += new System.EventHandler(this.cinzaToolStripMenuItem_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(661, 470);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "FormMain";
+            this.Text = "OCR";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.ToolStripButton toolStripButton3;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton1;
+        private System.Windows.Forms.ToolStripMenuItem crop1ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem crop2ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem processarFiltrosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cinzaToolStripMenuItem;
+    }
+}
---
+++ OCR/OCRTest/FormMain.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Imaging;
+using AForge.Imaging.Filters;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace OCRTest
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+            String ip = "172.16.102.13";
+            t1 = new TcpListener(ip, 4243);
+            cliente = default(TcpClient);
+            t1.Start();
+            cliente = t1.AcceptTcpClient();
+
+        }
+
+        TcpListener t1;
+        Socket skt;
+        NetworkStream ns;
+        StreamReader sr;
+        Thread th;
+        TcpClient cliente;
+        String mensagem = "Retornou";
+
+        public void retornar() {
+            ns = cliente.GetStream();
+            byte[] buffer = Encoding.ASCII.GetBytes(mensagem);
+            ns.Write(buffer, 0, buffer.Length);
+        }
+        
+
+        string GetIpAdress()
+        {
+            IPHostEntry host;
+            string localip = "?";
+            host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (IPAddress ip in host.AddressList)
+            {
+                if (ip.AddressFamily.ToString() == "InterNetwork")
+                {
+                    localip = ip.ToString();
+                }
+            }
+            return localip;
+        }
+        void ReceiveImage()
+        {
+            try
+            {
+                t1 = new TcpListener(4242);
+                t1.Start();
+                skt = t1.AcceptSocket();
+                ns = new NetworkStream(skt);
+                pictureBox1.Image = System.Drawing.Image.FromStream(ns);
+                t1.Stop();
+                if (skt.Connected == true)
+                {
+                    while (true)
+                    {
+                        ReceiveImage();
+                    }
+                }
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+            retornar();
+        }
+
+        public Bitmap currentImage { get; set; }
+
+        public Bitmap process(Bitmap bmp)
+        {
+            Bitmap newImage = (Bitmap)bmp.Clone();
+            newImage = new AForge.Imaging.Filters.GrayscaleY().Apply(newImage);
+            newImage = new AForge.Imaging.Filters.Threshold(50).Apply(newImage);
+            newImage = new AForge.Imaging.Filters.Median().Apply(newImage);
+            newImage = new AForge.Imaging.Filters.Median().Apply(newImage);
+            newImage = new AForge.Imaging.Filters.Dilatation().Apply(newImage);
+            newImage = new AForge.Imaging.Filters.Median().Apply(newImage);
+            newImage = new AForge.Imaging.Filters.Erosion().Apply(newImage);
+            newImage = new AForge.Imaging.Filters.Erosion().Apply(newImage);
+            //newImage = filter.Apply(newImage);
+            // newImage = filter.Apply(newImage);
+            // Erosion filter2 = new Erosion();
+            // newImage = filter2.Apply(newImage);
+            //newImage = filter2.Apply(newImage);
+            // newImage = filter2.Apply(newImage);
+            return newImage;
+        }
+
+        private void cortar(int x, int y, int w, int z)
+        {
+            var img = (Bitmap)pictureBox1.Image.Clone();
+            int a, b;
+            a = (img.Width) / x;
+            b = (img.Height) / y;
+            Crop filter = new Crop(new Rectangle(a, b, w, z));
+            var newImage = (Bitmap)filter.Apply(img);
+            currentImage = newImage;
+            pictureBox1.Image = newImage;
+        }
+        private void cortar2(int y, int w, int z)
+        {
+            var img = (Bitmap)pictureBox1.Image.Clone();
+            int a, b;
+            a = 0;
+            b = (img.Height) / y;
+            Crop filter = new Crop(new Rectangle(a, b, w, z));
+            var newImage = (Bitmap)filter.Apply(img);
+            currentImage = newImage;
+            pictureBox1.Image = newImage;
+        }
+        private void cortar3(int x, int w, int z)
+        {
+            var img = (Bitmap)pictureBox1.Image.Clone();
+            Int32 a, b;
+            a = (img.Width) / x;
+            b = 0;
+            Crop filter = new Crop(new Rectangle(a, b, w, z));
+            var newImage = (Bitmap)filter.Apply(img);
+            currentImage = newImage;
+            pictureBox1.Image = newImage;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog(this) == DialogResult.OK)
+            {
+                var img = System.Drawing.Image.FromFile(openFileDialog.FileName);
+                pictureBox1.Image = img;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var image = (Bitmap) pictureBox1.Image;
+            var ocr = new tessnet2.Tesseract();
+            ocr.Init(@"tessdata", "eng", true);
+            ocr.SetVariable("tesseract_char_whitelist", "ABCDEFGHIJKLMNOPQRSTUVXWYZ-1234567890");
+            var result = ocr.DoOCR(image, Rectangle.Empty);
+            StringBuilder sb = new StringBuilder();
+            foreach (tessnet2.Word word in result)
+                sb.Append(word.Text + " ");
+            textBox1.Text = sb.ToString();
+        }
+
+        private void toolStripStatusLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            th = new Thread(new ThreadStart(ReceiveImage));
+            th.Start();
+        }
+
+        private void toolStripButton3_Click(object sender, EventArgs e)
+        {
+            var img = (Bitmap)pictureBox1.Image.Clone();
+            var showImg = (Bitmap)pictureBox1.Image.Clone();
+            //img = Grayscale.CommonAlgorithms.Y.Apply(img);
+            img = new OtsuThreshold().Apply(img);
+            img = new Erosion().Apply(img);
+            img = new Invert().Apply(img);
+
+            BlobCounter bc = new BlobCounter();
+            bc.BackgroundThreshold = Color.Black;
+            bc.ProcessImage(img);
+            MessageBox.Show(String.Format("The image contains {0} objects.", bc.ObjectsCount));
+
+            Rectangle rect = new Rectangle(0, 0, showImg.Width, showImg.Height);
+            BitmapData bmpData = showImg.LockBits(rect, ImageLockMode.ReadWrite, showImg.PixelFormat);
+            
+            bc.GetObjectsRectangles().ToList().ForEach(i =>
+            {
+                Crop filter = new Crop(new Rectangle(i.X, i.Y, 230,75));
+                var img2 = (Bitmap)filter.Apply(img);
+                img2 = new Invert().Apply(img2);
+                //Drawing.Rectangle(bmpData, i, Color.Black);
+                var ocr = new tessnet2.Tesseract();
+                ocr.SetVariable("tesseract_char_whitelist", "ABCDEFGHIJKLMNOPQRSTUVXWYZ-1234567890");
+                ocr.Init(@"tessdata", "eng", false);
+                var result = ocr.DoOCR(img2, Rectangle.Empty);
+                StringBuilder sb = new StringBuilder();
+                foreach (tessnet2.Word word in result)
+                    sb.Append(word.Text + " ");
+                //try
+                //{
+
+                //    string line = String.Format(sb.ToString());
+                //    byte[] buffer = Encoding.ASCII.GetBytes(line);
+                //    client = new TcpClient(GetIpAdress(), 4243);
+                //    ns = client.GetStream();
+                //    br = new BinaryWriter(ns);
+                //    //BinaryFormatter bf = new BinaryFormatter();
+                //    //Bitmap newImage = (Bitmap)bf.Deserialize(ns);
+                //    //pictureBox1.Image = newImage;
+                //    br.Write(buffer);
+                //    br.Close();
+                //    ns.Close();
+                //    client.Close();
+
+                //}
+                //catch (Exception ex) { MessageBox.Show(ex.Message); }
+                MessageBox.Show("?"/*String.Format(sb.ToString())*/);
+                textBox1.Text = sb.ToString();
+                pictureBox1.Image = img2;
+               
+            });
+
+            showImg.UnlockBits(bmpData);
+
+            pictureBox1.Image = img;
+        }
+
+        private void crop1ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            cortar(3, 3, 600, 400);
+            cortar(3, 3, 350, 250);
+            cortar2(2, 350, 125);
+            cortar2(3, 350, 70);
+            cortar3(7, 220, 70);
+        }
+
+        private void crop2ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            cortar(3, 3, 600, 400);
+            cortar(3, 3, 350, 250);
+          
+        }
+
+        private void processarFiltrosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var img = (Bitmap)pictureBox1.Image.Clone();
+           // var showImg = (Bitmap)currentImage.Clone();
+            img = process(img);
+            currentImage = img;
+            pictureBox1.Image = currentImage;
+        }
+
+        private void cinzaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var img = (Bitmap)pictureBox1.Image.Clone();
+            img = Grayscale.CommonAlgorithms.Y.Apply(img);
+            pictureBox1.Image = img;
+        }
+    }
+}
